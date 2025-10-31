4b06e0fd3e33a69e0b4fc0d596d09859b597ebe4
4pzj94zx77-maker <4pzj94zx77@privaterelay.appleid.com>
2025-10-31 00:49:11 +0000

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -35,7 +35,7 @@ document.getElementById("calcular").addEventListener("click", () => {
   }
   else if (tipo === "terrenos") {
     // Terrenos e outros imóveis urbanos
-    imt = valor * 0.065; // taxa fixa 6,5% (AT 2025)
+    imt = valor * 0.050; // taxa fixa 6,5% (AT 2025)
   }
 
   if (imt < 0) imt = 0;
